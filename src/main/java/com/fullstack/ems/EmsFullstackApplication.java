788cd02d1711fe043b3e8eefc3cf259913143fa0
myusername <udutha.bhargav@hotmail.com>
2025-03-02 21:31:09 -0600

    Initial commit

---
+++ src/main/java/com/fullstack/ems/EmsFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsFullstackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fullstack/ems/entities/Employee.java
@@ -0,0 +1,73 @@
+package com.fullstack.ems.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/*@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter*/
+@Entity
+@Table(name="employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", unique = true, nullable = false)
+    private String email;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
